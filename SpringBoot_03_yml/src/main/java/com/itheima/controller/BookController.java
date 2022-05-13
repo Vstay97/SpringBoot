6c52e881d2a957808b9ea37611613c4f3cef4d26
Vsaty <583429957@qq.com>
2022-05-14 01:36:19 +0800

    使用environment获得所有属性; 使用Java类获取部分属性

--- SpringBoot_03_yml/src/main/java/com/itheima/controller/BookController.java
+++ SpringBoot_03_yml/src/main/java/com/itheima/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.itheima.controller;
 
+import com.itheima.MyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +32,11 @@ public class BookController {
     @Value("${currDir}")
     private String dir1;
 
+    @Autowired
+    private Environment env;
 
+    @Autowired
+    private MyDataSource dataSource;
 
 
     // 获得映射
@@ -37,14 +44,16 @@ public class BookController {
     public String getById() {
         System.out.println("hello,world");
 
-        System.out.println("country =====>>> "+country1);
-        System.out.println("user.name =====>>> "+name1);
-        System.out.println("likes[0] =====>>> "+likes1);
-        System.out.println("user[0].age =====>>> "+age1);
-        System.out.println("dir1 =====>>> "+dir1);
-
-
-
+        System.out.println("country =====>>> " + country1);
+        System.out.println("user.name =====>>> " + name1);
+        System.out.println("likes[0] =====>>> " + likes1);
+        System.out.println("user[0].age =====>>> " + age1);
+        System.out.println("dir1 =====>>> " + dir1);
+        System.out.println("-----------------------------");
+        System.out.println("env:name =======>>> " + env.getProperty("user2.name"));
+        System.out.println("-----------------------------");
+        System.out.println(dataSource);
+        System.out.println("dataSource.driver: "+dataSource.getDriver());
 
 
         return "hello,world";
