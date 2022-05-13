d8fa1431fc77dba2259f3729e546080e308b448e
Vsaty <583429957@qq.com>
2022-05-13 23:52:45 +0800

    读取yml数据格式

---
+++ SpringBoot_03_yml/src/main/java/com/itheima/SpringBoot0101QuickstartApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import com.itheima.controller.BookController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBoot0101QuickstartApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringBoot0101QuickstartApplication.class, args);
+
+    }
+}
---
+++ SpringBoot_03_yml/src/main/java/com/itheima/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.itheima.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// REST模式
+// @RestController = @Controller + @ResponseBody
+// @Controller: 注入服务
+// @ResponseBody: 设置当前控制器方法响应内容为当前返回值，无需解析
+@RestController
+// 访问路径
+@RequestMapping("/books")
+public class BookController {
+
+    @Value("${country}")
+    private String country1;
+
+    @Value("${users1.name}")
+    private String name1;
+
+    @Value("${likes[0]}")
+    private String likes1;
+
+    @Value("${users[0].age}")
+    private String age1;
+
+
+
+
+    // 获得映射
+    @GetMapping
+    public String getById() {
+        System.out.println("hello,world");
+
+        System.out.println("country =====>>> "+country1);
+        System.out.println("user.name =====>>> "+name1);
+        System.out.println("likes[0] =====>>> "+likes1);
+        System.out.println("user[0].age =====>>> "+age1);
+
+
+
+
+
+        return "hello,world";
+    }
+}
