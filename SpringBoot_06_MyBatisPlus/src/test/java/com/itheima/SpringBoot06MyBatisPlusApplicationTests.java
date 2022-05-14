67e339eefbe488ef71ca4a0f5c79073d628e21c0
Vsaty <583429957@qq.com>
2022-05-14 21:31:01 +0800

    SpringBoot整合MyBatisPlus: 1. 定义实体类 2. 创建mapper接口 3. 配置文件中,配置数据库中 表的前缀

--- SpringBoot_06_MyBatisPlus/src/test/java/com/itheima/SpringBoot06MyBatisPlusApplicationTests.java
+++ SpringBoot_06_MyBatisPlus/src/test/java/com/itheima/SpringBoot06MyBatisPlusApplicationTests.java
@@ -1,13 +1,20 @@
 package com.itheima;
 
+import com.itheima.dao.UserDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBoot06MyBatisPlusApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
     void contextLoads() {
+        System.out.println(userDao.selectById(23));
+        System.out.println(userDao.selectList(null));
     }
 
 }
