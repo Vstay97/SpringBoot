edda6df2f8bef94f1dacd3bf6460345db5a13c3e
Vsaty <583429957@qq.com>
2022-05-14 20:17:09 +0800

    SpringBoot整合MyBatisPlus:建立SpringBoot工程,勾选MySQL

---
+++ SpringBoot_06_MyBatisPlus/src/main/java/com/itheima/SpringBoot06MyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06MyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06MyBatisPlusApplication.class, args);
+    }
+
+}
