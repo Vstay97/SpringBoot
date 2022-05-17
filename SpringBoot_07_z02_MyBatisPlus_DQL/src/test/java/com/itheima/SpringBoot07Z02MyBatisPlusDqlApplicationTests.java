689765d2134dd98bd9bef613c31ff72a78ab8f19
Vsaty <583429957@qq.com>
2022-05-18 02:28:23 +0800

    SpringBoot整合MyBatisPlus：DQL 修复依赖错误，框架搭建完成。创建实体类、mapper接口

--- SpringBoot_07_z02_MyBatisPlus_DQL/src/test/java/com/itheima/SpringBoot07Z02MyBatisPlusDqlApplicationTests.java
+++ SpringBoot_07_z02_MyBatisPlus_DQL/src/test/java/com/itheima/SpringBoot07Z02MyBatisPlusDqlApplicationTests.java
@@ -1,13 +1,23 @@
 package com.itheima;
 
+import com.itheima.dao.UserDao;
+import com.itheima.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringBoot07Z02MyBatisPlusDqlApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
-    void contextLoads() {
+    void testGetAll(){
+        List<User> userList = userDao.selectList(null);
+        System.out.println(userList);
     }
 
 }
