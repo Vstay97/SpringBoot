1d32f6033ee09564afd4cd0269b3833b97c14ed5
Vsaty <583429957@qq.com>
2022-05-18 02:14:43 +0800

    SpringBoot整合MyBatisPlus：DQL 创建框架，导入依赖lombok druid mp mysql

---
+++ SpringBoot_07_z02_MyBatisPlus_DQL/src/main/java/com/itheima/SpringBoot07Z02MyBatisPlusDqlApplication.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import com.itheima.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07Z02MyBatisPlusDqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07Z02MyBatisPlusDqlApplication.class, args);
+    }
+
+}
