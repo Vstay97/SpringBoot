7241338df95afc2dc4a902f199d3c34a718bb266
Vsaty <583429957@qq.com>
2022-05-18 14:36:18 +0800

    SpringBoot整合MyBatisPlus：DQL 三种条件查询格式

--- SpringBoot_07_z02_MyBatisPlus_DQL/src/test/java/com/itheima/SpringBoot07Z02MyBatisPlusDqlApplicationTests.java
+++ SpringBoot_07_z02_MyBatisPlus_DQL/src/test/java/com/itheima/SpringBoot07Z02MyBatisPlusDqlApplicationTests.java
@@ -1,5 +1,7 @@
 package com.itheima;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itheima.dao.UserDao;
 import com.itheima.domain.User;
 import org.junit.jupiter.api.Test;
@@ -16,8 +18,31 @@ class SpringBoot07Z02MyBatisPlusDqlApplicationTests {
 
     @Test
     void testGetAll(){
-        List<User> userList = userDao.selectList(null);
-        System.out.println(userList);
+        /*
+         * 第一种 通过列名，直接查
+         */
+        // QueryWrapper<User> qw = new QueryWrapper<>();
+        // qw.lt("age",18);
+        // List<User> userList = userDao.selectList(qw);
+        // System.out.println(userList);
+
+        /**
+         * 第二种，通过lambda表达式，指定属性查
+         */
+        // QueryWrapper<User> qw = new QueryWrapper<>();
+        // qw.lambda().lt(User::getAge,18);
+        // List<User> userList = userDao.selectList(qw);
+        // System.out.println(userList);
+
+        /**
+         * 第三种，通过lambda对象查
+         */
+        LambdaQueryWrapper<User> lqw = new LambdaQueryWrapper<>();
+        lqw.lt(User::getAge,18);
+        userDao.selectList(lqw);
+
+
+
     }
 
 }
