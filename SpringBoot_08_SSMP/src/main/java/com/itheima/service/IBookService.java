4935914ef020324f13c61ef2fb438f981a02f435
Vsaty <583429957@qq.com>
2022-05-20 14:54:20 +0800

    SpringBoot整合SSMP：表现层标准开发

--- SpringBoot_08_SSMP/src/main/java/com/itheima/service/IBookService.java
+++ SpringBoot_08_SSMP/src/main/java/com/itheima/service/IBookService.java
@@ -1,7 +1,9 @@
 package com.itheima.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.itheima.domain.Book;
 
 public interface IBookService extends IService<Book> {
+    public IPage<Book> getPage(Integer current,Integer size);
 }
--- SpringBoot_08_SSMP/src/main/java/com/itheima/service/impl/IBookServiceImpl.java
+++ SpringBoot_08_SSMP/src/main/java/com/itheima/service/impl/IBookServiceImpl.java
@@ -1,13 +1,25 @@
 package com.itheima.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.dao.BookDao;
 import com.itheima.domain.Book;
 import com.itheima.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IBookServiceImpl extends ServiceImpl<BookDao,Book> implements IBookService {
+    @Autowired
+    private BookDao bookDao;
 
+    @Override
+    public IPage<Book> getPage(Integer current, Integer size) {
+
+        IPage<Book> page = new Page(current,size);
+        bookDao.selectPage(page,null);
+        return page;
+    }
 }
